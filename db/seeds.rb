296d8a0c22c9a4ea64c42c538d1de312b977a4ff
Lilas Marchand <lilasviolettemarchand@gmail.com>
2021-12-26 18:33:55 +0100

    portfolio card

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@ puts 'Destroy portfolio'
 Portfolio.destroy_all
 
 puts 'Adding portfolio'
-Portfolio.create(name: 'Perfect Watch', company: 'Nicolas, Sixtine, Brice & myself', description: 'Website developped with Le Wagon school, concept was to help users to find a movie in 5 minutes depending on their platforms', url: 'https://www.perfect-watch.net/', thumbnail: 'none')
-Portfolio.create(name: 'Sitter on the way', company: 'Nicolas, Sixtine, Brice & myself', description: 'Website developped with Le Wagon school, concept was to develop a website based on a marketplace as AirBnb', url: 'https://sitter-on-the-way.herokuapp.com/', thumbnail: 'none')
-Portfolio.create(name: 'Laurence et Jean Leccia', company: 'Les Ateliers Leccia', description: 'This website was created for two painters that wanted to promote their paintings and have a minimalist way to display their work.', url: 'https://laurenceetjeanleccia.com/', thumbnail: 'none')
-Portfolio.create(name: 'Les Ateliers Leccia', company: 'Les Ateliers Leccia', description: 'A school that propose differents type of services, such as painting and sewing.', url: 'https://lesateliersleccia.com/', thumbnail: 'none')
+Portfolio.create(name: 'Perfect Watch', company: 'Nicolas, Sixtine, Brice & myself', description: 'Website developped with Le Wagon school, concept was to help users to find a movie in 5 minutes depending on their platforms', url: 'https://www.perfect-watch.net/', thumbnail: 'PerfectWatch.png')
+Portfolio.create(name: 'Sitter on the way', company: 'Nicolas, Sixtine, Brice & myself', description: 'Website developped with Le Wagon school, concept was to develop a website based on a marketplace as AirBnb', url: 'https://sitter-on-the-way.herokuapp.com/', thumbnail: 'SitterOnTheWay.png')
+Portfolio.create(name: 'Laurence et Jean Leccia', company: 'Les Ateliers Leccia', description: 'This website was created for two painters that wanted to promote their paintings and have a minimalist way to display their work.', url: 'https://laurenceetjeanleccia.com/', thumbnail: 'none.jpeg')
+Portfolio.create(name: 'Les Ateliers Leccia', company: 'Les Ateliers Leccia', description: 'A school that propose differents type of services, such as painting and sewing.', url: 'https://lesateliersleccia.com/', thumbnail: 'none.jpeg')
 puts 'Portfolio added'
