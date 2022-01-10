cfb94e0b3054658269a3428af63b734c861c04cf
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-01-10 16:32:40 +0100

    new seeds

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'download_pdf', to: "pages#download_pdf"
   resources :portfolios, only: [:index, :show]
+  resources :contacts, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
