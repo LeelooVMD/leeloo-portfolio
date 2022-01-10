d37acdbfcb184107fdb17c22431836cd484ae990
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-01-10 16:13:17 +0100

    download cv available

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def download_pdf
+    send_file "#{Rails.root}/app/assets/data/CV_Lilas_Marchand.pdf", type: "application/pdf", x_sendfile: true
+  end
 end
