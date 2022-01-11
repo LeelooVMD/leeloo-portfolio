c09982c8cccfdfcb481efa34b6aa3962e3573d9b
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-01-11 12:31:06 +0100

    adding style on links

--- app/javascript/controllers/displayer_controller.js
+++ app/javascript/controllers/displayer_controller.js
@@ -4,10 +4,11 @@ export default class extends Controller {
   static targets = ['content']
 
   tabs() {
-    // console.log(this.contentTargets)
+    // gaaconsole.log(this.contentTargets)
     const links = this.contentTargets
     // this.contentTargets.classList.remove('d-none')
     links.forEach((link) => {
+      link.classList.add('load')
       link.classList.remove('d-none')
     })
   // your logic (controller actions)
