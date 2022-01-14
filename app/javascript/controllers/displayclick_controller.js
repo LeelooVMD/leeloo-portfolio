18e30fd03494615bbaeb4d186db4468f0491770f
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-01-14 14:20:25 +0100

    stimulus shaking pic

--- app/javascript/controllers/displayclick_controller.js
+++ app/javascript/controllers/displayclick_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = ['pic']
+
   connect() {
     setTimeout(() => this.picTarget.classList.add('pic-shake'), 2000);
   };
