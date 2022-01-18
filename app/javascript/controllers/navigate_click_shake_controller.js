41496cae7468d0c9f72ae6a7e6975be2981d7c73
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-01-18 15:55:51 +0100

    data-value

--- app/javascript/controllers/navigate_click_shake_controller.js
+++ app/javascript/controllers/navigate_click_shake_controller.js
@@ -3,44 +3,33 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ['pic', 'content', 'navigate']
   static value = {
-    shake: Boolean
+    help: Boolean
   }
 
 
   tabs() {
+    // console.log(this.helpValue)
+    // this.data.set('shake', false)
+    this.helpValue = 'false'
     const links = this.contentTargets
     links.forEach((link) => {
       link.classList.add('load')
       link.classList.toggle('d-none')
     })
-    this.navigateTarget.classList.add('d-none')
   };
-
-  // this.shakevalue = false
-
   // methode shake avec le shake et le remove
   // quel est la valeur de shake value si c'est true elle fait sinon elle fait rien
   // nouveau data action qui ecoute le click avec nouvelle methode qui passe le shake value à false
 
   shake() {
-    if (this.shakeValue == 'true')
-    this.picTarget.classList.add('pic-shake');
-    this.navigateTarget.classList.remove('d-none');
+    if (this.helpValue) {
+      this.picTarget.classList.add('pic-shake');
+      this.navigateTarget.classList.add('load');
+      this.navigateTarget.classList.remove('d-none');
+    }
   };
 
   connect() {
-    // const targets = this.contentTargets
-    // targets.forEach((target) => {
-      // if (tabs()) {
-      //   setTimeout(() => this.picTarget.classList.add('pic-shake'), 2500);
-      //   this.navigateTarget.classList.add('load');
-      //   setTimeout(() => this.navigateTarget.classList.remove('d-none'), 2500);
-      // }
-      if (this.contentTarget.classList.contains('d-none')) {
-        // console.log(target.classList)
-        setTimeout(() => this.shake(), 2500);
-        this.navigateTarget.classList.add('load');
-      }
-    // })
+      setTimeout(() => this.shake(), 2500);
   };
 }
