51f989cae184d4a07e3028ecc3121b4972f4ade5
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-01-18 16:55:58 +0100

    mail_form working

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'download_pdf', to: "pages#download_pdf"
   resources :portfolios, only: [:index, :show]
-  resources :connections, only: [:index, :show] do
-    resources :contacts, only: [:new, :create]
-  end
+  resources :connections, only: [:index, :show]
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
