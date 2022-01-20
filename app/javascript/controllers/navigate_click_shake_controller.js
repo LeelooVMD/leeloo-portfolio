97e0ad3fb574a44832a8c02d67b0524eee3e27c6
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-01-20 10:34:36 +0100

    Bundler fix

--- app/javascript/controllers/navigate_click_shake_controller.js
+++ app/javascript/controllers/navigate_click_shake_controller.js
@@ -6,10 +6,7 @@ export default class extends Controller {
     help: Boolean
   }
 
-
   tabs() {
-    // console.log(this.helpValue)
-    // this.data.set('shake', false)
     this.helpValue = 'false'
     const links = this.contentTargets
     links.forEach((link) => {
@@ -17,9 +14,6 @@ export default class extends Controller {
       link.classList.toggle('d-none')
     })
   };
-  // methode shake avec le shake et le remove
-  // quel est la valeur de shake value si c'est true elle fait sinon elle fait rien
-  // nouveau data action qui ecoute le click avec nouvelle methode qui passe le shake value à false
 
   shake() {
     if (this.helpValue) {
