3f1546ca83995e5b477bd1a3c276922b877e4c9e
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-01-24 17:38:22 +0100

    still fixing css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,7 +29,7 @@ import "controllers";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
+// document.addEventListener('turbolinks:load', () => {
+//   // Call your functions here, e.g:
+//   // initSelect2();
+// });
